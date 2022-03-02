0a0c224cc2da1f5739e74852f86b972ae0ae1094
Igors78 <oleinikovsigors@gmail.com>
2022-03-02 07:36:24 +0200

    Call static and nonstatic methods from the flow

---
+++ java_method/src/main/java/com/method/Concat.java
@@ -0,0 +1,12 @@
+package com.method;
+
+public class Concat {
+	public static String conca(String a, String b) {
+		return (a + " " + b);
+	}
+	
+	public String multCon(String a, String b, String c)
+	{
+		return (a + " " + b + c);
+	}
+}
